6c394ebb9b6129be8dbc75a0b2658fee23fdb15f
Jemshit Iskenderov <jemshit@mail.com>
2025-03-19 18:46:52 -0400

    * Protect task routes using Authentication

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { getTasks,createTask, getTaskById, updateTask, deleteTask } from "../controllers/taskControllers.js";
+import { getTasks, createTask, getTaskById, updateTask, deleteTask } from "../controllers/taskControllers.js";
+import { AuthGuard } from "../middleware/authMiddleware.js";
 
 const taskRouter = express.Router();
 
-taskRouter.get('/',getTasks);//LIST of the Tasks
-taskRouter.post('/',createTask);// CREATE New Task
-taskRouter.get('/:id',getTaskById)// Getting Specific Task
-taskRouter.put('/:id',updateTask)// Updating Specific Task
-taskRouter.delete('/:id',deleteTask)// Delete Specific Task
+taskRouter.get('/', AuthGuard, getTasks);       // LIST of the Tasks
+taskRouter.post('/', AuthGuard, createTask);    // CREATE New Task
+taskRouter.get('/:id', AuthGuard, getTaskById)  // Getting Specific Task
+taskRouter.put('/:id', AuthGuard, updateTask)   // Updating Specific Task
+taskRouter.delete('/:id', AuthGuard, deleteTask)// Delete Specific Task
 
 
 export default taskRouter;
