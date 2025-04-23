88019e15f29848ca9d226fa779866215cf6198c0
mahmadsadikRepositry <sadikkoliya@gmail.com>
2025-04-23 12:39:28 -0400

    Remove default route

--- src/app.js
+++ src/app.js
@@ -12,11 +12,6 @@ app.use(cors())
 app.use(morgan('dev'))
 
 //Routes
-//Health Check
-
-app.use('/',()=>{
-    console.log(`Welcome to the Task Management Page.`)
-});
-
+//Health Check 
 app.use('/api/tasks', taskRouter);
 export default app;
